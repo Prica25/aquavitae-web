4aee5082524c3453674255ada27ee8e802200dbc
Prica <prica_@outlook.pt>
2022-11-23 22:30:20 +0000

    syn

---
+++ src/components/nutricionalPlan/foodSearch.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-select
+    filled
+    v-model="model"
+    multiple
+    :options="options"
+    :loading="loading"
+    @virtual-scroll="onScroll"
+  />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {}
+  },
+})
+</script>
